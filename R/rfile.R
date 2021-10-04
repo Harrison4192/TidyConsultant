cd8ecb7654644f47cade822bc4dcdc611a10f0ef
Harrison Tietze <Harrison4192@gmail.com>
2021-10-04 19:54:27 +0900

    fix2

---
+++ R/rfile.R
@@ -0,0 +1,14 @@
+#' hello world
+#'
+#' Test function to pass cmd check
+#'
+#' @return none
+#' @export
+#'
+#' @examples
+#'
+#' hello_world()
+hello_world <- function(){
+
+  print("Welcome to TidyConsultant")
+}
